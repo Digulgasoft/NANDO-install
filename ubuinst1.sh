32b92863edb838ef0315113767d94c11e46db7c8
Digulgasoft <102381192+Digulgasoft@users.noreply.github.com>
2022-07-16 13:12:18 -0300

    Update ubuinst1.sh

--- ubuinst1.sh
+++ ubuinst1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /bin || exit
-wget raw.githubusercontent.com/Digulgasoft/NANDO-PWEB/painel/install/ubuinst.sh && chmod +x ubuinst.sh && dos2unix ubuinst.sh > /dev/null 2>&1
+wget raw.githubusercontent.com/Digulgasoft/NANDO-PWEB/install/ubuinst.sh && chmod +x ubuinst.sh && dos2unix ubuinst.sh > /dev/null 2>&1
 cd || exit
