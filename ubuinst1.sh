b17526809382f242bad77e1f92c07dac75394742
Digulgasoft <102381192+Digulgasoft@users.noreply.github.com>
2022-07-16 14:03:17 -0300

    Update ubuinst1.sh

--- ubuinst1.sh
+++ ubuinst1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo 'wget raw.githubusercontent.com/Digulgasoft/NANDO-PWEB/install/ubuinst.sh && chmod +x ubuinst.sh && dos2unix ubuinst.sh > /dev/null 2>&1'  bash
+echo 'wget raw.githubusercontent.com/Digulgasoft/NANDO-PWEB/install/master/ubuinst.sh && chmod +x ubuinst.sh && dos2unix ubuinst.sh > /dev/null 2>&1'  bash
 /bin/ubuinst.sh
