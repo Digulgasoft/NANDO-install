6ca3ec2810fede35f6fe2b4d50308e89905a3bd6
Digulgasoft <102381192+Digulgasoft@users.noreply.github.com>
2022-07-16 13:41:00 -0300

    Update ubuinst3.sh

--- ubuinst3.sh
+++ ubuinst3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cat /dev/null > ~/.bash_history && history -c
 rm /bin/ubuinst* > /dev/null 2>&1
-echo 'wget raw.githubusercontent.com/Digulgasoft/NANDO-install/bad/ubuinst2.sh && chmod +x ubuinst2.sh && dos2unix ubuinst2.sh > /dev/null 2>&1'  bash
+echo 'wget raw.githubusercontent.com/Digulgasoft/NANDO-install/master/ubuinst2.sh && chmod +x ubuinst2.sh && dos2unix ubuinst2.sh > /dev/null 2>&1'  bash
 /bin/ubuinst2.sh
